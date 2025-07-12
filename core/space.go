d16a87adce88f2c3624d2ea9cb73a99123e1c22e
Jannis Metrikat <120120832+jmetrikat@users.noreply.github.com>
2025-07-12 11:36:35 +0200

    Add command to list available spaces and enhance account selection handling

--- core/space.go
+++ core/space.go
@@ -6,7 +6,9 @@ import (
 
 	"github.com/anyproto/anytype-heart/pb"
 	"github.com/anyproto/anytype-heart/pb/service"
+	"github.com/anyproto/anytype-heart/pkg/lib/bundle"
 	"github.com/anyproto/anytype-heart/pkg/lib/pb/model"
+	"github.com/anyproto/anytype-heart/util/pbtypes"
 )
 
 func ApproveJoinRequest(spaceID, identity string, permissions model.ParticipantPermissions) error {
@@ -87,4 +89,90 @@ func ViewSpaceInvite(inviteCID, inviteFileKey string) (*SpaceInviteInfo, error) 
 		return nil
 	})
 	return info, err
+}
+
+type SpaceListItem struct {
+	SpaceID string
+	Name    string
+	Status  model.SpaceStatus
+}
+
+// ListSpaces returns a list of all available spaces
+func ListSpaces() ([]SpaceListItem, error) {
+	techSpaceID, err := GetStoredTechSpaceID()
+	if err != nil {
+		return nil, fmt.Errorf("tech space ID not found in config - please login first: %w", err)
+	}
+
+	var spaces []SpaceListItem
+	err = GRPCCall(func(ctx context.Context, client service.ClientCommandsClient) error {
+		req := &pb.RpcObjectSearchRequest{
+			SpaceId: techSpaceID,
+			Filters: []*model.BlockContentDataviewFilter{
+				{
+					RelationKey: bundle.RelationKeyResolvedLayout.String(),
+					Condition:   model.BlockContentDataviewFilter_Equal,
+					Value:       pbtypes.Int64(int64(model.ObjectType_spaceView)),
+				},
+				{
+					RelationKey: bundle.RelationKeySpaceLocalStatus.String(),
+					Condition:   model.BlockContentDataviewFilter_In,
+					Value:       pbtypes.IntList(int(model.SpaceStatus_Unknown), int(model.SpaceStatus_Ok)),
+				},
+				{
+					RelationKey: bundle.RelationKeySpaceAccountStatus.String(),
+					Condition:   model.BlockContentDataviewFilter_In,
+					Value:       pbtypes.IntList(int(model.SpaceStatus_Unknown), int(model.SpaceStatus_SpaceActive)),
+				},
+			},
+			Sorts: []*model.BlockContentDataviewSort{
+				{
+					RelationKey:    bundle.RelationKeySpaceOrder.String(),
+					Type:           model.BlockContentDataviewSort_Asc,
+					NoCollate:      true,
+					EmptyPlacement: model.BlockContentDataviewSort_End,
+				},
+			},
+			Keys: []string{
+				bundle.RelationKeyTargetSpaceId.String(),
+				bundle.RelationKeyName.String(),
+				bundle.RelationKeySpaceLocalStatus.String(),
+			},
+		}
+
+		resp, err := client.ObjectSearch(ctx, req)
+		if err != nil {
+			return fmt.Errorf("failed to search spaces: %w", err)
+		}
+		if resp.Error != nil && resp.Error.Code != pb.RpcObjectSearchResponseError_NULL {
+			return fmt.Errorf("object search error: %s", resp.Error.Description)
+		}
+
+		for _, record := range resp.Records {
+			item := SpaceListItem{}
+
+			// Get space ID
+			if spaceIDVal := pbtypes.GetString(record, bundle.RelationKeyTargetSpaceId.String()); spaceIDVal != "" {
+				item.SpaceID = spaceIDVal
+			}
+
+			// Get name
+			if nameVal := pbtypes.GetString(record, bundle.RelationKeyName.String()); nameVal != "" {
+				item.Name = nameVal
+			}
+
+			// Get status
+			if statusVal := pbtypes.GetInt64(record, bundle.RelationKeySpaceLocalStatus.String()); statusVal != 0 {
+				item.Status = model.SpaceStatus(statusVal)
+			}
+
+			if item.SpaceID != "" {
+				spaces = append(spaces, item)
+			}
+		}
+
+		return nil
+	})
+
+	return spaces, err
 }
